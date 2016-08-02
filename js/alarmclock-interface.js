384dd23b1a90116351e078ee3268b1925982c775
Noah Yasskin and Rachael Bermudes <noah@>
2016-08-02 15:14:31 -0700

    add time that updates automatically

--- js/alarmclock-interface.js
+++ js/alarmclock-interface.js
@@ -1,25 +1,15 @@
 var AlarmClock = require('./../js/alarmclock.js').alarmModule;
 
+var datetime = null;
+        date = null
+
+var update = function() {
+  date = moment(new Date());
+  datetime.html(date.format('h:mm:ss a'));
+};
+
 $(document).ready(function(){
-  $('#current-time').text(moment().format('h:mm:ss a'));
-
-  // function update() {
-  //   $('#current-time').html(moment().format('hh:mm:ss a'));
-  // }
-  //
-  // setInterval(update, 1000);
-  // $("form#alarm-form").submit(function() {
-  //   event.preventDefault();
-  //   var alarmTime = $ ("input#alarm-time").val();
-  //   time = moment(timeInput, "hh:mm a");
-  //   var newAlarm = new Alarm(time);
-  //   var checkAlarm = setInterval (function()) {
-  //     if (newAlarm.alarmTurnOn()) {
-  //       alert("ALARM!");
-  //       clearInterval(checkAlarm);
-  //     }
-  //   }, 1000);
-  //   $('.alarm-output').text(alarmTime);
-  // });
-
+  datetime = $('#current-time')
+  update();
+  setInterval(update, 1000);
 });
