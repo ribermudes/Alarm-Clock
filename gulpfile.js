ef92d6279e87bb80d406f2c1c9582c2503b0b9d2
Noah Yasskin and Rachael Bermudes <noah@>
2016-08-02 13:08:07 -0700

    add gulp clean task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
+var del = require('del');
 
 var buildProduction = utilities.env.production;
 
@@ -26,7 +27,11 @@ gulp.task("minifyScripts", ["jsBrowserify"], function(){
     .pipe(gulp.dest("./build/js"));
 });
 
-gulp.task("build", function(){
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function(){
   if (buildProduction) {
     gulp.start('minifyScripts');
   } else {
